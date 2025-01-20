d035fad5a0732c73f48108e0aa9d959b210aad9e
damian9112 <kopec.damian@icloud.com>
2025-01-20 21:38:42 +0100

    Add Table Invoice and index view modification

--- invoices/views.py
+++ invoices/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Invoice
 
 # Create your views here.
 # Invoices
 def index(request):
-    return HttpResponse("Invoices page.")
+    all_invoices = Invoice.objects.all()
+    output = ", ".join([invoice.invoice_number for invoice in all_invoices])
+    return HttpResponse(output)
 
 # Detail
 def detail(request, invoice_id):
     return HttpResponse("Invoice detail page.")
 
+# PDF
 def pdf(request, invoice_id):
     return HttpResponse("Invoice download page.")
