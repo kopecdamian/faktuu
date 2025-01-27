468c5bfcf46a9d3c58206deb08c23db196a99bad
damian9112 <kopec.damian@icloud.com>
2025-01-27 23:56:57 +0100

    Add forms view to add invoice

--- invoices/views.py
+++ invoices/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Invoice
+from django.urls import reverse
+from datetime import datetime
 
 # Create your views here.
 # Invoices
@@ -12,6 +13,22 @@ def index(request):
     }
     return render(request, "invoices/index.html", context)
     
+# Create invoice
+def forms(request):
+    return render(request, "invoices/forms.html")
+
+# Save invoice
+def save(request):
+    if request.method == 'POST':
+        
+        client = request.POST.get('client')
+        product = request.POST.get('product')
+        
+        Invoice.objects.create(
+            client = client,
+            product = product
+        )
+    return HttpResponseRedirect(reverse("invoices:index"))
 
 # Detail
 def detail(request, invoice_id):
@@ -20,4 +37,8 @@ def detail(request, invoice_id):
 
 # PDF
 def pdf(request, invoice_id):
+    
+    # year = datetime.now().year
+    # month = datetime.now().month
+    # print(Invoice.objects.filter(invoice_number__icontains=f"{year}/{month:02}"))
     return HttpResponse("Invoice download page.")
