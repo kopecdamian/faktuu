446abcc2e5a45a45f1c2cd55c2f6dbca2769aba6
damian9112 <kopec.damian@icloud.com>
2025-01-27 23:56:57 +0100

    Add forms view to add invoice

--- invoices/urls.py
+++ invoices/urls.py
@@ -6,6 +6,10 @@ app_name = "invoices"
 urlpatterns = [
     # /invoices
     path("invoices/", views.index, name="index"),
+     # /invoice/create
+    path("invoices/create", views.forms, name="forms"),
+    # /invoice/save
+    path("invoices/save", views.save, name="save"),
     # /invoice/5
     path("invoices/<int:invoice_id>/", views.detail, name="detail"),
     # /invoice/5/pdf
