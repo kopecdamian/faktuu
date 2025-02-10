9a8dcb6d8b5f6333172c0bdcfb394f210d3b1210
damian9112 <kopec.damian@icloud.com>
2025-02-10 23:15:32 +0100

    Add InvoiceProduct table

--- invoices/admin.py
+++ invoices/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Client, Product, Invoice, InvoiceCounter
+from .models import Client, Product, Invoice, InvoiceCounter, InvoiceProduct
 
 admin.site.register(Client)
 admin.site.register(Product)
 admin.site.register(Invoice)
+admin.site.register(InvoiceProduct)
 admin.site.register(InvoiceCounter)
