33e590bad0ade7dc1047eb6930f7183877dc7458
damian9112 <kopec.damian@icloud.com>
2025-02-12 22:24:42 +0100

    Add invoice detail view with invoice and its products

--- invoices/models.py
+++ invoices/models.py
@@ -37,13 +37,14 @@ class Invoice(models.Model):
         Client,
         on_delete=models.CASCADE
     )
-    product = models.CharField(max_length=255)
+    total_netto = models.DecimalField(max_digits=10, decimal_places=2)
+    total_brutto = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.invoice_number
     
 class InvoiceProduct(models.Model):
-    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE, related_name="products")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     def __str__(self):
