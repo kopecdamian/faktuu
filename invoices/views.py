315b81bc91b6517a7d1aeacce3c683bec5148b55
damian9112 <kopec.damian@icloud.com>
2025-02-20 23:57:50 +0100

    Modify Invoice Form

--- invoices/views.py
+++ invoices/views.py
@@ -21,47 +21,24 @@ def invoices(request):
 def invoiceForm(request):
     if request.method == "POST":
         form = InvoiceForm(request.POST)
-        formset = InvoiceProductFormSet(request.POST)
-        if form.is_valid() and formset.is_valid():
+        if form.is_valid():
             invoice = form.save()
-            products = formset.save(commit=False)
-            for product in products:
-                product.invoice = invoice
-                product.save()
-            return HttpResponseRedirect(reverse("invoices:invoices"))
+            print(request.POST)
+            productsName = request.POST.getlist("productName[]")
+            productsPrice = request.POST.getlist("productPrice[]")
+            productsQuantity = request.POST.getlist("productQuantity[]")
+            for i in range(len(productsName)):
+                InvoiceProduct.objects.create(
+                    invoice = invoice,
+                    name = productsName[i],
+                    price = productsPrice[i],
+                    quantity = int(productsQuantity[i])
+                )
+        return HttpResponseRedirect(reverse("invoices:invoices"))
     else:
         form = InvoiceForm()
-        formset = InvoiceProductFormSet()
+    return render(request, "invoices/invoiceForm.html", {"form": form})
 
-    return render(request, "invoices/invoiceForm.html", {"form": form, "formset":formset})
-    # return render(request, "invoices/forms.html", context)
-
-# Save invoice
-# def save(request):
-#     if request.method == 'POST':
-
-#         # compare last invoice number with current date and create new invoice number
-#         current_year = datetime.now().year
-#         current_month = f"{datetime.now().month:02}"
-#         old_invoice_number = InvoiceCounter.objects.filter(client = "Damian").first().highest_number
-#         invoice_prefix, invoice_year, invoice_month, document_number = old_invoice_number.split("/")
-#         if(int(invoice_year) == int(current_year) and current_month == invoice_month):
-#             new_document_number = int(document_number) + 1
-#             new_invoice_number = f"FV/{current_year}/{current_month}/{new_document_number}"
-#         else:
-#             new_invoice_number = f"FV/{current_year}/{current_month}/1"
-        
-#         # save new invoice number in database
-#         InvoiceCounter.objects.filter(client = "Damian").update(highest_number=new_invoice_number)
-#         client = request.POST.get('client')
-#         product = request.POST.get('product')
-        
-#         Invoice.objects.create(
-#             invoice_number = new_invoice_number,
-#             client = Client.objects.get(id=client),
-#             product = product
-#         )
-#     return HttpResponseRedirect(reverse("invoices:index"))
 
 # Detail
 @login_required()
@@ -121,32 +98,6 @@ def clientform(request):
         form = ClientForm()
 
     return render(request, "invoices/clientform.html", {"form": form})
-    # return render(request, "invoices/clientform.html")
-
-# Save New Client
-# def saveclient(request):
-#     if request.method == 'POST':
-
-#         clientName = request.POST.get('clientName')
-#         nipNumber = request.POST.get('nipNumber')
-#         adress = request.POST.get('adress')
-#         city = request.POST.get('city')
-#         postalCode = request.POST.get('postalCode')
-#         country = request.POST.get('country')
-#         phoneNumber = request.POST.get('phoneNumber')
-#         email = request.POST.get('email')
-        
-#         Client.objects.create(
-#             name = clientName,
-#             nip_number = nipNumber,
-#             street = adress,
-#             city = city,
-#             postal_code = postalCode,
-#             country = country,
-#             phone_number = phoneNumber,
-#             email = email
-#         )
-#     return HttpResponseRedirect(reverse("invoices:clients"))
 
 # Client Detail
 @login_required()
@@ -192,24 +143,6 @@ def productForm(request):
         form = ProductForm()
 
     return render(request, "invoices/productForm.html", {"form": form})
-    # return render(request, "invoices/productForm.html")
-
-# Save New Product
-# def productSave(request):
-#     if request.method == 'POST':
-
-#         productName = request.POST.get('productName')
-#         vatRate = request.POST.get('vatRate')
-#         priceNetto = request.POST.get('priceNetto')
-#         priceBrutto = request.POST.get('priceBrutto')
-        
-#         Product.objects.create(
-#             name = productName,
-#             vat_rate = vatRate,
-#             price_netto = priceNetto,
-#             price_brutto = priceBrutto
-#         )
-#     return HttpResponseRedirect(reverse("invoices:products"))
 
 # Product Detail
 @login_required()
