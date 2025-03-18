ea714bc9b377828785b72cf393cf2a64342d36d5
damian9112 <kopec.damian@icloud.com>
2025-03-18 17:00:38 +0100

    styles: modify form style [#42]

--- invoices/forms.py
+++ invoices/forms.py
@@ -5,12 +5,44 @@ from .models import Product, Client, Invoice, InvoiceProduct
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'price_netto', 'vat_rate', 'price_brutto']
+        fields = ['name', 'price_netto', 'tax', 'price_brutto']
+        labels = {
+            "name":  "Nazwa",
+            "price_netto": "Cena Netto",
+            "tax": "Podatek VAT",
+            "price_brutto": "Cena Brutto",
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj nazwę produktu'}),
+            'price_netto': forms.NumberInput(attrs={'class': 'custom-input', 'placeholder': 'Cena netto'}),
+            'tax': forms.NumberInput(attrs={'class': 'custom-input', 'placeholder': 'Podatek VAT'}),
+            'price_brutto': forms.NumberInput(attrs={'class': 'custom-input', 'placeholder': 'Cena brutto'}),
+        }
 
 class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = ['name', 'nip_number', 'street', 'city', 'postal_code', 'country', 'phone_number', 'email']
+        labels = {
+            "name":  "Nazwa",
+            "nip_number": "Numer NIP",
+            "street": "Ulica",
+            "city": "Miasto",
+            "postal_code": "Kod Pocztowy",
+            "country": "Kraj",
+            "phone_number": "Numer Telefonu",
+            "email": "Adres Email",
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj nazwę klienta'}),
+            'nip_number': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj NIP'}),
+            'street': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj ulicę'}),
+            'city': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj miasto'}),
+            'postal_code': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj kod pocztowy'}),
+            'country': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj kraj'}),
+            'phone_number': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj numer telefonu'}),
+            'email': forms.EmailInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj adres e-mail'}),
+        }
 
 class InvoiceForm(forms.ModelForm):
     class Meta:
