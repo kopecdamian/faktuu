c084560d25cc9d6b21e779745a01e1aea21631a4
damian9112 <kopec.damian@icloud.com>
2025-03-22 01:48:28 +0100

    styles: modify form style [#46}

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,4 +13,24 @@ class CustomUserCreationForm(UserCreationForm):
 class CustomUserForm(forms.ModelForm):
     class Meta:
         model = CustomUser
-        fields = ['nip_number', 'regon_number', 'bank_account_number']
+        fields = ['nip_number', 'regon_number', 'bank_account_number', 'year', 'street', 'city', 'postal_code', 'country']
+        labels = {
+            "nip_number":  "NIP",
+            "regon_number": "REGON",
+            "bank_account_number": "Numer konta",
+            "year": "Rok",
+            "street": "Ulica",
+            "city": "Miasto",
+            "postal_code": "Kod pocztowy",
+            "country": "Kraj"
+        }
+        widgets = {
+            'nip_number': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj numer bankowy'}),
+            'regon_number': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj numer bankowy'}),
+            'bank_account_number': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj numer bankowy'}),
+            'year': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj numer bankowy'}),
+            'street': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj numer bankowy'}),
+            'city': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj numer bankowy'}),
+            'postal_code': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj numer bankowy'}),
+            'country': forms.TextInput(attrs={'class': 'custom-input', 'placeholder': 'Podaj numer bankowy'}),
+        }
