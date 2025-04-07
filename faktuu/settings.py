1012754562ee1bbded85f416f959655da14cf9f1
kopecdamian <kopec.damian@icloud.com>
2025-04-07 10:52:52 +0000

    Refactor: moved sensitive data to .env v2

--- faktuu/settings.py
+++ faktuu/settings.py
@@ -29,12 +29,12 @@ env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env(DJANGO_SECRET_KEY)
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = env(ALLOWED_HOSTS).split(',')
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(',')
 
 
 # Application definition
