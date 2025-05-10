13b7c23d2df96dab48cf674c7f2fab61a61c2ddd
kopecdamian <kopec.damian@icloud.com>
2025-05-10 13:10:21 +0000

    config: Update Django settings to use PostgreSQL instead of SQLite [#52]

--- faktuu/settings.py
+++ faktuu/settings.py
@@ -88,8 +88,15 @@ WSGI_APPLICATION = 'faktuu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+                'options': '-c search_path=myschema'
+            },
+        'NAME': env('POSTGRES_NAME'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASS'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT')
     }
 }
 
